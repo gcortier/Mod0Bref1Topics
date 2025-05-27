903ee81d89d25b21e389c81eb4b44ce89a3bcfee
gcortier <contact@gcortier.com>
2025-05-27 13:21:10 +0200

    Add routes unit tests

--- test_sentiment_api.py
+++ test_sentiment_api.py
@@ -11,25 +11,13 @@ def test_root_route():
 
 
 # def test_analyse_sentiment_positive():
-#     response = client.post("/analyse_sentiment/", json={"texte": "C'est génial!"})
-#     assert response.status_code == 200
-#     data = response.json()
-#     assert "pos" in data and data["pos"] > 0
+response = client.post("/analyse_sentiment/", json={"texte": "i'm happy"})
+assert response.status_code == 200
+data = response.json()
+assert "pos" in data and data["pos"] > 0
 
 # def test_analyse_sentiment_negative():
-#     response = client.post("/analyse_sentiment/", json={"texte": "C'est horrible."})
-#     assert response.status_code == 200
-#     data = response.json()
-#     assert "neg" in data and data["neg"] > 0
-
-# def test_analyse_sentiment_neutral():
-#     response = client.post("/analyse_sentiment/", json={"texte": "Ceci est une phrase."})
-#     assert response.status_code == 200
-#     data = response.json()
-#     assert "neu" in data and data["neu"] > 0
-
-# def test_analyse_sentiment_empty():
-#     response = client.post("/analyse_sentiment/", json={"texte": ""})
-#     assert response.status_code == 200
-#     data = response.json()
-#     assert isinstance(data, dict)
+response = client.post("/analyse_sentiment/", json={"texte": "i'm sad"})
+assert response.status_code == 200
+data = response.json()
+assert "neg" in data and data["neg"] > 0
